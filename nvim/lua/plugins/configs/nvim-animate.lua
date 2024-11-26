0cef65754295140d62488c4e7c70c4d67dcabeed
joscarranzs <jcarranzsosa@gmail.com>
2024-11-26 18:46:08 -0500

    Improvements in animations

--- nvim/lua/plugins/configs/nvim-animate.lua
+++ nvim/lua/plugins/configs/nvim-animate.lua
@@ -2,8 +2,41 @@ return {
     "echasnovski/mini.animate",
     event = "VeryLazy",
     opts = function(_, opts)
+        -- Animación del cursor
+        opts.cursor = {
+            enable = true,
+            timing = require("mini.animate").gen_timing.linear({ duration = 20 }),
+            path = require("mini.animate").gen_path.line(),
+        }
+
+        -- Animación de desplazamiento vertical
         opts.scroll = {
-            enable = false,
+            enable = true,
+            timing = require("mini.animate").gen_timing.linear({ duration = 20 }),
+            subscroll = require("mini.animate").gen_subscroll.equal({ steps = 60 }),
+        }
+
+        -- Animación de redimensionado de ventanas
+        opts.resize = {
+            enable = true,
+            timing = require("mini.animate").gen_timing.linear({ duration = 20 }),
+            subresize = require("mini.animate").gen_subresize.equal(),
+        }
+
+        -- Animación de apertura de ventanas
+        opts.open = {
+            enable = true,
+            timing = require("mini.animate").gen_timing.linear({ duration = 20 }),
+            winconfig = require("mini.animate").gen_winconfig.static({ step_count = 25 }),
+            winblend = require("mini.animate").gen_winblend.linear({ from = 80, to = 100, steps = 60 }),
+        }
+
+        -- Animación de cierre de ventanas
+        opts.close = {
+            enable = true,
+            timing = require("mini.animate").gen_timing.linear({ duration = 20 }),
+            winconfig = require("mini.animate").gen_winconfig.static({ step_count = 25 }),
+            winblend = require("mini.animate").gen_winblend.linear({ from = 100, to = 80, steps = 60 }),
         }
     end,
 }
