777ff2ddcf8b9d6c28520a5f5bbed084e0ddab96
joscarranzs <jcarranzsosa@gmail.com>
2024-12-01 04:03:22 -0500

    Fixed some problems with fish configuration.

--- fish/config.fish
+++ fish/config.fish
@@ -8,8 +8,7 @@ alias ls="lsd"
 alias la="lsd -a"
 alias ll="lsd -l"
 alias lla="lsd -l -a"
-alias vfz="vim (fzf)"
-alias lg='lazygit'
+alias lg="lazygit"
 alias create="mkdir"
 alias delete="rm -rf"
 alias back="cd .."
@@ -17,12 +16,29 @@ alias tm="tmux"
 alias kill="tmux kill-server"
 command -qv nvim && alias vim nvim
 
-# Evaluate Homebrew env only if it exists
-if test -x /home/linuxbrew/.linuxbrew/bin/brew
-    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+# Función para abrir Vim directamente en la interfaz FZF
+function vfz
+    fzf | read -l selected  # Captura la salida de `fzf` directamente en la variable `selected`
+    if test -n "$selected"  # Verifica si se seleccionó algo
+        vim $selected       # Abre el archivo seleccionado en Vim
+    end
 end
 
-# Función para abrir Neovim directamente en la interfaz DBUI (Database User Interface)
+# Función para abrir Neovim directamente en la interfaz DBUI(Database User Interface)
 function nvimdb
     nvim -c DBUI  # Abre Neovim y ejecuta el comando DBUI para iniciar la interfaz de base de datos
+end
+
+# Configuración de Homebrew(directamente desde brew shellenv)
+set --global --export HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
+set --global --export HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar"
+set --global --export HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew"
+fish_add_path --global --move --path "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin"
+
+if test -n "$MANPATH[1]"
+    set --global --export MANPATH '' "$MANPATH"
+end
+
+if not contains "/home/linuxbrew/.linuxbrew/share/info" "$INFOPATH"
+    set --global --export INFOPATH "/home/linuxbrew/.linuxbrew/share/info" "$INFOPATH"
 end
